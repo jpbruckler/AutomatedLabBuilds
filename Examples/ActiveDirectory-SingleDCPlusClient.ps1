f53d25428b5fa5210f2127f887282d77e898e9ba
John <jpbruckler@gmail.com>
2024-02-01 01:51:54 -0500

    [Espanso]: An error occurred during rendering, please examine the logs for more information.

---
+++ Examples/ActiveDirectory-SingleDCPlusClient.ps1
@@ -0,0 +1,11 @@
+#Here AL installs a lab with one domain controller and one client. The OS can be configured quite easily as well as
+#the domain name or memory. AL takes care about network settings like in the previous samples.
+
+New-LabDefinition -Name Lab1 -DefaultVirtualizationEngine HyperV
+
+Add-LabMachineDefinition -Name DC1 -Memory 1GB -OperatingSystem 'Windows Server 2022 Datacenter Evaluation (Desktop Experience)' -Roles RootDC -DomainName contoso.com
+Add-LabMachineDefinition -Name Client1 -Memory 1GB -OperatingSystem 'Windows 11 Pro' -DomainName contoso.com
+
+Install-Lab
+
+Show-LabDeploymentSummary -Detailed
--- PowerShellUniversal/psu-lab.ps1
+++ PowerShellUniversal/psu-lab.ps1
@@ -8,58 +8,64 @@
     1 member server will have PowerShell Universall installed and configured.
 #>
 
-$labName        = 'psudev'
-$labDomain      = 'psudev.local'
-$labInstallUser = 'Install'
-$labInstallPW   = 'gulf-tango-delta'
+$AlName         = 'psudev'
+$DomainName     = 'psudev.local'
+$InstallUser    = 'Install'
+$InstallPW      = 'gulf-tango-delta'
 
 
 # Create an empty lab template, define the network and domain, and set the installation credentials
-New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
-Add-LabDomainDefinition -Name $labDomain -AdminUser $labInstallUser -AdminPassword $labInstallPW
-Set-LabInstallationCredential -Username $labInstallUser -Password $labInstallPW
+New-LabDefinition -Name $AlName -DefaultVirtualizationEngine HyperV
+Add-LabDomainDefinition -Name $DomainName -AdminUser $InstallUser -AdminPassword $InstallPW
+Set-LabInstallationCredential -Username $InstallUser -Password $InstallPW
 
-Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.77.0/24
-Add-LabVirtualNetworkDefinition -Name 'Default Switch' -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Wi-Fi' }
+Add-LabVirtualNetworkDefinition -Name $AlName -AddressSpace 192.168.77.0/24 -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Wi-Fi' }
 
 
 # defining default parameter values, as these ones are the same for all the machines
 $PSDefaultParameterValues = @{
-    'Add-LabMachineDefinition:Network' = $labName
-    'Add-LabMachineDefinition:ToolsPath'= "$labSources\Tools"
-    'Add-LabMachineDefinition:DnsServer1'= '192.168.77.10'
-    'Add-LabMachineDefinition:Gateway' = '192.168.77.10'
-    'Add-LabMachineDefinition:Memory'= 1GB
-    'Add-LabMachineDefinition:DomainName'= $labDomain
-    'Add-LabMachineDefinition:OperatingSystem'= 'Windows Server 2022 Datacenter Evaluation (Desktop Experience)'
+    'Add-LabMachineDefinition:Network'          = $AlName
+    'Add-LabMachineDefinition:ToolsPath'        = "$labSources\Tools"
+    'Add-LabMachineDefinition:DnsServer1'       = '192.168.77.10'
+    'Add-LabMachineDefinition:Memory'           = 1GB
+    'Add-LabMachineDefinition:DomainName'       = $DomainName
+    'Add-LabMachineDefinition:OperatingSystem'  = 'Windows Server 2022 Datacenter Evaluation (Desktop Experience)'
 }
 
 # Create Root Domain Controller
 $postInstall = Get-LabPostInstallationActivity -ScriptFileName 'PrepPSUDevDC.ps1' -DependencyFolder $labSources\PostInstallationActivities\PsuDev\Domain
-$netAdapter = @()
-$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName -Ipv4Address 192.168.77.10
-$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch 'Default Switch' -UseDhcp
-Add-LabMachineDefinition -Name 'SVR-DC-01' -Roles RootDC,Routing -NetworkAdapter $netAdapter -PostInstallationActivity $postInstall
+Add-LabMachineDefinition -Name 'SVR-DC-01' -Roles RootDC -IpAddress 192.168.77.10 -PostInstallationActivity $postInstall
 
 # Create CA Root Server
-$roleCaRoot = Get-LabMachineRoleDefinition -Role CaRoot
+$roleCaRoot = Get-LabMachineRoleDefinition -Role CaRoot @{
+    CACommonName        = 'PsuDevRootCA1'
+    KeyLength           = '4096'
+    ValidityPeriod      = "Yearss"
+    ValidityPeriodUnits = "20"
+}
 Add-LabMachineDefinition -Name 'SVR-CA-01' -Roles $roleCaRoot -IpAddress 192.168.77.20 
 
 # Create Subordinate CA Server
-$role = Get-LabMachineRoleDefinition -Role CaSubordinate
+$role = Get-LabMachineRoleDefinition -Role CaSubordinate @{
+    CACommonName        = 'PsuDevSubCA1'
+    KeyLength           = '2048'
+    ValidityPeriod      = 'Years'
+    ValidityPeriodUnits = '8' 
+}
 Add-LabMachineDefinition -Name 'SVR-CA-02' -IpAddress 192.168.77.21 -Roles $role
 
 # Create PowerShell Universal Member Server
-Add-LabMachineDefinition -Name 'SVR-PSU-01' -IpAddress 192.168.77.30 -Memory 4GB
+Add-LabDiskDefinition -Name DataDrive -DiskSizeInGb 100 -Label Data -DriveLetter D -AllocationUnitSize 64kb
+Add-LabMachineDefinition -Name 'SVR-PSU-01' -IpAddress 192.168.77.30 -Memory 4GB -DiskName DataDrive
 
 # Create Windows 11 Client
 Add-LabMachineDefinition -Name 'END-WIN11-01' -OperatingSystem 'Windows 11 Pro'
 
 
-Install-Lab -NetworkSwitches -BaseImages -VMs
-Install-Lab -Domains
-Install-Lab -CA
-Install-Lab
+#Install-Lab -NetworkSwitches -BaseImages -VMs #-Verbose
+#Install-Lab -Domains -Verbose
+#Install-Lab -CA #-Verbose
+Install-Lab #-Verbose
 
 Enable-LabCertificateAutoenrollment -Computer -User -CodeSigning
 Show-LabDeploymentSummary -Detailed
